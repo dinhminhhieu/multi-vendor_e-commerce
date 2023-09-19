7be988d2a3a53f57b908eebf55e63d46833c4775
dinhminhhieu <dinhminhhieuvn@gmail.com>
2023-09-19 12:41:34 +0700

    4. design dashboard (P3)

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import icons from "../../assets/icons";
 import Chart from "react-apexcharts";
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
   const { FaMoneyCheck, GrProductHunt, AiOutlineShoppingCart, HiUserGroup } =
@@ -9,15 +10,15 @@ const AdminDashboard = () => {
   const state = {
     series: [
       {
-        name: "Đơn Hàng",
+        name: "Đơn hàng",
         data: [34, 65, 343, 65, 34, 34, 34, 56, 23, 67, 23, 45],
       },
       {
-        name: "Doanh Thu",
+        name: "Doanh thu",
         data: [34, 32, 45, 32, 34, 34, 43, 56, 23, 67, 23, 45],
       },
       {
-        name: "Người Bán",
+        name: "Người bán",
         data: [94, 65, 343, 65, 34, 34, 34, 56, 23, 67, 23, 45],
       },
     ],
@@ -60,6 +61,37 @@ const AdminDashboard = () => {
       legend: {
         position: "top",
       },
+      responsive: [
+        {
+          breakpoint: 565,
+          xaxis: {
+            categories: [
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apl",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ],
+          },
+          options: {
+            plotOptions: {
+              bar: {
+                horizontal: true,
+              },
+            },
+            chart: {
+              height: "550px",
+            },
+          },
+        },
+      ],
     },
   };
 
@@ -116,6 +148,153 @@ const AdminDashboard = () => {
               height={350}
             />
           </div>
+        </div>
+        <div className="w-full lg:w-5/12 lg:pl-4 mt-6 lg:mt-0">
+          <div className="w-full bg-[#283046] p-4 rounded-md text-white">
+            <div className="flex justify-between items-center">
+              <h2 className="font-semibold text-base text-white">
+                Tin nhắn gần đây
+              </h2>
+              <Link className="font-semibold text-sm text-[#cccccc]">
+                Xem tất cả
+              </Link>
+            </div>
+            <div className="flex flex-col gap-2 pt-6 text-white">
+              <ol className="relative border-1 border-slate-600 ml-4">
+                <li className="mb-3 ml-6">
+                  <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] rounded-full z-10 bg-[#000000]">
+                    <img
+                      src="http://localhost:3000/images/admin.png"
+                      alt=""
+                      className="rounded-full w-full h-full shadow-lg"
+                    />
+                  </div>
+                  <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
+                    <div className="flex justify-between items-center mb-2">
+                      <Link className="text-sm font-normal">admin</Link>
+                      <time className="mb-1 text-sm font-normal sm:order-last">
+                        4 ngày trước
+                      </time>
+                    </div>
+                    <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
+                      Xin chào
+                    </div>
+                  </div>
+                </li>
+
+                <li className="mb-3 ml-6">
+                  <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] rounded-full z-10 bg-[#000000]">
+                    <img
+                      src="http://localhost:3000/images/admin.png"
+                      alt=""
+                      className="rounded-full w-full h-full shadow-lg"
+                    />
+                  </div>
+                  <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
+                    <div className="flex justify-between items-center mb-2">
+                      <Link className="text-sm font-normal">admin</Link>
+                      <time className="mb-1 text-sm font-normal sm:order-last">
+                        4 ngày trước
+                      </time>
+                    </div>
+                    <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
+                      Xin chào
+                    </div>
+                  </div>
+                </li>
+
+                <li className="mb-3 ml-6">
+                  <div className="flex absolute -left-5 shadow-lg justify-center items-center w-10 h-10 p-[6px] rounded-full z-10 bg-[#000000]">
+                    <img
+                      src="http://localhost:3000/images/admin.png"
+                      alt=""
+                      className="rounded-full w-full h-full shadow-lg"
+                    />
+                  </div>
+                  <div className="p-3 bg-slate-800 rounded-lg border border-slate-600 shadow-sm">
+                    <div className="flex justify-between items-center mb-2">
+                      <Link className="text-sm font-normal">admin</Link>
+                      <time className="mb-1 text-sm font-normal sm:order-last">
+                        4 ngày trước
+                      </time>
+                    </div>
+                    <div className="p-2 text-xs font-normal bg-slate-700 rounded-lg border border-slate-800">
+                      Xin chào
+                    </div>
+                  </div>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full p-4 bg-[#283046] rounded-md mt-6">
+        <div className="flex justify-between items-center">
+          <h2 className="font-semibold text-base text-white">
+            Đơn hàng gần đây
+          </h2>
+          <Link className="font-semibold text-sm text-[#cccccc]">
+            Xem tất cả
+          </Link>
+        </div>
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left text-white">
+            <thead className="text-sm text-white uppercase border-b border-slate-700">
+              <tr>
+                <th className="py-3 px-4" scope="col">
+                  Mã đơn hàng
+                </th>
+                <th className="py-3 px-4" scope="col">
+                  Đơn giá
+                </th>
+                <th className="py-3 px-4" scope="col">
+                  Trạng thái
+                </th>
+                <th className="py-3 px-4" scope="col">
+                  Tình trạng đơn hàng
+                </th>
+                <th className="py-3 px-4" scope="col">
+                  Hành động
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {[1, 2, 3, , 4, 5].map((d, i) => (
+                <tr key={i}>
+                  <td
+                    className="py-3 px-4 font-medium whitespace-nowrap"
+                    scope="col"
+                  >
+                    #1
+                  </td>
+                  <td
+                    className="py-3 px-4 font-medium whitespace-nowrap"
+                    scope="col"
+                  >
+                    10.000
+                  </td>
+                  <td
+                    className="py-3 px-4 font-medium whitespace-nowrap"
+                    scope="col"
+                  >
+                    <span>Chưa xử lý</span>
+                  </td>
+                  <td
+                    className="py-3 px-4 font-medium whitespace-nowrap"
+                    scope="col"
+                  >
+                    <span>Chưa xử lý</span>
+                  </td>
+                  <td
+                    className="py-3 px-4 font-medium whitespace-nowrap"
+                    scope="col"
+                  >
+                    <Link>Xem</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
