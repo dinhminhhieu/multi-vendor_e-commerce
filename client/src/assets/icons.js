47c7fe7121e0d4422eaeb125258ef5a1e01f14fa
dinhminhhieu <dinhminhhieuvn@gmail.com>
2023-10-05 17:19:27 +0700

    34. header design tailwindcss react (P2)

--- client/src/assets/icons.js
+++ client/src/assets/icons.js
@@ -1,19 +1,24 @@
-import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineMail, AiFillHeart } from "react-icons/ai";
 import { FaFacebookF, FaUser, FaLock, FaList } from "react-icons/fa";
 import { GrInstagram } from "react-icons/gr";
 import { BsTwitter, BsGithub } from "react-icons/bs";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { FaCartShopping } from "react-icons/fa6";
+import { IoIosCall } from "react-icons/io";
 
 const icons = {
   AiOutlineMail,
+  AiFillHeart,
   FaFacebookF,
   FaUser,
   FaLock,
   FaList,
+  FaCartShopping,
   GrInstagram,
   BsTwitter,
   BsGithub,
   MdOutlineKeyboardArrowDown,
+  IoIosCall,
 };
 
 export default icons
