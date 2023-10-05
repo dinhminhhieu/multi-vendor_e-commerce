5d6fb8dc5afe8507382c879626e47c339490ba7f
dinhminhhieu <dinhminhhieuvn@gmail.com>
2023-10-05 18:08:43 +0700

    35. header design tailwindcss react (P3)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,8 +20,17 @@ const Header = () => {
 
   const user = true;
   const [showSideBar, setShowSideBar] = useState(true);
+  const [categoryShow, setCategoryShow] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [category, setCategory] = useState("");
   const { pathname } = useLocation();
   const wishlist = 4;
+  const categorys = [
+    "Điện thoại",
+    "Quần - Áo",
+    "Giày - Dép",
+    "Phụ kiện công nghệ",
+  ];
 
   return (
     <div className="w-full bg-white">
@@ -319,6 +328,95 @@ const Header = () => {
               </li>
               <span className="text-sm font-medium">Welcome to SnapDeal</span>
             </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-[85%] lg:w-[90%] mx-auto">
+        <div className="flex w-full flex-wrap md-lg:gap-8">
+          <div className="w-3/12 md-lg:w-full">
+            <div className="bg-white relative">
+              <div
+                onClick={() => setCategoryShow(!categoryShow)}
+                className="h-[50px] bg-red-500 text-white flex justify-center md-lg:justify-between md-lg:px-6 items-center gap-3 font-bold text-md cursor-pointer"
+              >
+                <div className="flex justify-center items-center gap-3">
+                  <span>
+                    <FaList />
+                  </span>
+                  <span>Danh Mục Sản Phẩm</span>
+                </div>
+                <span className="pt-1">
+                  <MdOutlineKeyboardArrowDown size={20} />
+                </span>
+              </div>
+              <div
+                className={`${
+                  categoryShow ? "h-0" : "h-[400px]"
+                } overflow-hidden transition-all md-lg:relative duration-500 absolute z-[99999] bg-white w-full border-2 border-red-500`}
+              >
+                <ul className="py-2 text-slate-600 font-medium h-full overflow-auto">
+                  {categorys.map((c, i) => {
+                    return (
+                      <li
+                        key={i}
+                        className="flex justify-start items-center gap-2 px-[24px] py-[6px]"
+                      >
+                        <Link className="text-sm block">{c}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="w-9/12 pl-8 md-lg:pl-0 md-lg:w-full">
+            <div className="flex flex-wrap w-full justify-between items-center md-lg:gap-6">
+              <div className="w-8/12 md-lg:w-full">
+                <div className="flex border-red-500 border-2 h-[50px] items-center relative gap-5">
+                  <div className="relative after:absolute after:h-[25px] after:w-[1px] after:bg-red-500 after:-right-[15px] md:hidden">
+                    <select
+                      onChange={(e) => setCategory(e.target.value)}
+                      name=""
+                      id=""
+                      className="w-[180px] text-slate-600 font-semibold bg-transparent px-2 h-full outline-0 border-none"
+                    >
+                      <option value="">Chọn danh mục</option>
+                      {categorys.map((c, i) => (
+                        <option key={i} value={c}>
+                          {c}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <input
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    className="w-full relative bg-transparent text-slate-500 outline-0 px-3 h-full"
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Tìm kiếm sản phẩm..."
+                  />
+                  <button className="bg-red-500 right-0 absolute px-8 h-full font-semibold uppercase text-white">
+                    Search
+                  </button>
+                </div>
+              </div>
+              <div className="w-4/12 block md-lg:hidden pl-2 md-lg:w-full md-lg:pl-0">
+                <div className="w-full flex justify-end md-lg:justify-start gap-3 items-center">
+                  <div className="w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center">
+                    <span>
+                      <IoIosCall />
+                    </span>
+                  </div>
+                  <div className="flex justify-end flex-col gap-1">
+                    <h2 className="text-md font-medium text-slate-700">
+                      0386690205
+                    </h2>
+                    <span className="text-sm">Hỗ trợ 24/7</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
