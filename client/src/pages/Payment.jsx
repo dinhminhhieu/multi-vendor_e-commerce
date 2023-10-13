df93068d968831306b41be806c6f641cba942e09
dinhminhhieu <dinhminhhieuvn@gmail.com>
2023-10-14 00:32:38 +0700

    70. customer order place (P4)

---
+++ client/src/pages/Payment.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Payment = () => {
+  return (
+    <div>
+        <Header />
+        <section>
+            
+        </section>
+        <Footer />
+    </div>
+  )
+}
+
+export default Payment
--- server/controllers/order/orderController.js
+++ server/controllers/order/orderController.js
@@ -1,7 +1,76 @@
+const authOrder = require("../../models/authOrder");
+const customerOrder = require("../../models/customerOrder");
+const moment = require("moment");
+const {responseReturn} = require("../../utils/response")
+
 class orderController {
-  place_order = async(req, res) => {
-    console.log(req.body)
-  }
+  //1. Đặt hàng
+  place_order = async (req, res) => {
+    const { price, products, shipping_fee, shippingInfo, userId } = req.body;
+    let authorOrderData = [];
+    let cartId = [];
+    const tempDate = moment(Date.now()).format("LLL");
+    
+    let customerOrderProduct = [];
+
+    for (let i = 0; i < products.length; i++) {
+      const pro = products[i].products;
+      for (let j = 0; j < pro.length; j++) {
+        let tempCusPro = pro[j].productInfo;
+        tempCusPro.quantity = pro[j].quantity;
+        customerOrderProduct.push(tempCusPro);
+        if (pro[j]._id) {
+          cartId.push(pro[j]._id);
+        }
+      }
+    }
+    try {
+      const order = await customerOrder.create({
+        customerId: userId,
+        shippingInfo,
+        products: customerOrderProduct,
+        price: price + shipping_fee,
+        delivery_status: "pending",
+        payment_status: "unpaid",
+        date: tempDate,
+      });
+      for (let i = 0; i < products.length; i++) {
+        const pro = products[i].products;
+        const pri = products[i].price;
+        const sellerId = products[i].sellerId;
+        let storePro = [];
+        for (let j = 0; j < pro.length; j++) {
+          let tempPro = pro[j].productInfo;
+          tempPro.quantity = pro[j].quantity;
+          storePro.push(tempPro);
+        }
+
+        authOrderData.push({
+          orderId: order.id,
+          sellerId,
+          products: storePro,
+          price: pri,
+          payment_status: "unpaid",
+          shippingInfo: "Dhaka myshop Warehouse",
+          delivery_status: "pending",
+          date: tempDate,
+        });
+      }
+      await authOrderModel.insertMany(authOrderData);
+      for (let k = 0; k < cardId.length; k++) {
+        await cardModel.findByIdAndDelete(cardId[k]);
+      }
+      setTimeout(() => {
+        this.paymentCheck(order.id);
+      }, 15000);
+      responseReturn(res, 201, {
+        message: "Đặt hàng thành công!",
+        orderId: order.id,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 }
 
-module.exports = new orderController()
+module.exports = new orderController();
